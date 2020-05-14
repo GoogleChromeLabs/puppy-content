fde6724d725a4f563cc27dcb60affee785e7e139
Reilly Grant <reillyg@google.com>
2020-05-13 17:00:23 -0700

    Move #!/bin/sh to the top of create.sh
    This line needs to be the first thing in the file to work properly.

--- create.sh
+++ create.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh
 
 API=$1
 if [ "$2" = "-r" ]
